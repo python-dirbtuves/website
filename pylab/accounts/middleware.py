e2a998d69c071f07cce91dc4a73a035ec8991330
Albertas <albertas.gimbutas@mii.vu.lt>
2015-07-26 23:13:36 +0300

    Fix custom locale middleware

--- pylab/accounts/middleware.py
+++ pylab/accounts/middleware.py
@@ -4,11 +4,11 @@ from django.utils import translation
 
 class UserProfileLocaleMiddleware(LocaleMiddleware):
     def process_request(self, request):
-        super(UserProfileLocaleMiddleware, self).process_request(request)
-
         if (hasattr(request.user, 'userprofile') and
                 request.user.userprofile and
                 request.user.userprofile.language):
             language = request.user.userprofile.language
             translation.activate(language)
             request.LANGUAGE_CODE = language
+        else:
+            super(UserProfileLocaleMiddleware, self).process_request(request)
