9352b45f8c74a86e846726e4a35716743a8064fe
Mantas <sirexas@gmail.com>
2015-08-04 11:48:31 +0300

    Fix pylint errors

--- pylab/core/management.py
+++ pylab/core/management.py
@@ -2,7 +2,7 @@ from django.apps import apps
 from django.conf import settings
 
 
-def update_default_site(app_config, verbosity=2, **kwargs):
+def update_default_site(app_config, verbosity=2, **kwargs):  # pylint: disable=unused-argument
     Site = apps.get_model('sites', 'Site')
     site = Site.objects.get_current()
 
