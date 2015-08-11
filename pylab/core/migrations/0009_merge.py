06f68f686e2cf3b0189ca2ebecdffd9cdfd89717
iakovleva <paulina.iakovleva@gmail.com>
2015-08-11 11:41:46 +0300

    Merge migrations.

---
+++ pylab/core/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0007_auto_20150810_0155'),
+        ('core', '0008_auto_20150809_0654'),
+    ]
+
+    operations = [
+    ]
