8d2ba0fc2edfd125055e10fcf9d29567a698438e
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-18 13:19:25 +0300

    Show avatar in navbar for logged in users

--- pylab/accounts/models.py
+++ pylab/accounts/models.py
@@ -1,3 +1,6 @@
+import urllib
+import hashlib
+
 import django.contrib.auth.models as auth_models
 from django.conf import settings
 from django.db import models
@@ -9,6 +12,17 @@ from django.utils.translation import ugettext_lazy as _
 class UserProfile(models.Model):
     user = models.OneToOneField(auth_models.User, related_name='profile')
     language = models.CharField(_('Language'), max_length=7, choices=settings.LANGUAGES, default='', blank=True)
+
+    def get_avatar_url(self):
+        '''Returns avatar associated with email or generated identicon, see more:
+        https://secure.gravatar.com/site/implement/images/'''
+        if self.user.email:
+            user_hash = hashlib.md5(self.user.email.lower().encode('utf-8')).hexdigest()
+        else:
+            user_hash = hashlib.md5(self.user.username.lower().encode('utf-8')).hexdigest()
+        gravatar_url = "http://www.gravatar.com/avatar/%s?" % user_hash
+        gravatar_url += urllib.parse.urlencode({'d': 'identicon', 's': '24'})
+        return gravatar_url
 
 
 @receiver(post_save, sender=auth_models.User)
