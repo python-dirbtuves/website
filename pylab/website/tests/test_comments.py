d87d73b43a9a306d2140bfc6b15a056a6269da44
iakovleva <paulina.iakovleva@gmail.com>
2015-08-19 11:41:40 +0300

    Use WebTest instead of TestCase

--- pylab/website/tests/test_comments.py
+++ pylab/website/tests/test_comments.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.test import TestCase
+from django_webtest import WebTest
 
 from django.contrib.auth.models import User
 
@@ -9,27 +9,33 @@ from django_comments.models import Comment
 from pylab.core.models import Project
 
 
-class CommentsTests(TestCase):
+class CommentsTests(WebTest):
 
-    def test_add_comment(self):
-        user = User.objects.create_user('user1')
-        project = Project.objects.create(
-            author=user,
+    def setUp(self):
+        self.user = User.objects.create_user('u1')
+        self.project = Project.objects.create(
+            author=self.user,
             title='Test project',
             description='Description',
             created='2015-08-13'
         )
+
+    def test_add_comment(self):
+        resp = self.app.get('/projects/test-project/', user=self.user)
         now = datetime.datetime.now()
         comment = Comment.objects.create(
-            user_name='user2',
-            comment='test comment',
+            user=self.user,
+            comment='new comment',
             submit_date=now,
-            object_pk=project.id,
-            content_type_id=project.id,
+            object_pk=self.project.id,
+            content_type_id=self.project.id,
             site_id=1,
         )
         comment.save()
-        resp = self.client.post('/projects/test-project/', {'comment': comment})
+        resp = self.app.get('/projects/test-project/', user=self.user)
+        self.assertEqual(resp.status_int, 200)
 
-        self.assertEqual(resp.status_code, 200)
-#       self.assertContains(resp, 'test comment')
+        self.assertEqual(
+            list(Comment.objects.values_list('comment')),
+            [('new comment',)]
+        )
