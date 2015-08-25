89ee95bf33ce504377087de383f56e8582623738
iakovleva <paulina.iakovleva@gmail.com>
2015-08-25 09:48:04 +0300

    Add email parameter for creating test user

--- pylab/website/tests/test_comments.py
+++ pylab/website/tests/test_comments.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django_webtest import WebTest
 
 from django.contrib.auth.models import User
@@ -12,7 +10,7 @@ from pylab.core.models import Project
 class CommentsTests(WebTest):
 
     def setUp(self):
-        self.user = User.objects.create_user('u1')
+        self.user = User.objects.create_user('u1', email='test@example.com')
         self.project = Project.objects.create(
             author=self.user,
             title='Test project',
@@ -22,16 +20,9 @@ class CommentsTests(WebTest):
 
     def test_add_comment(self):
         resp = self.app.get('/projects/test-project/', user=self.user)
-        now = datetime.datetime.now()
-        comment = Comment.objects.create(
-            user=self.user,
-            comment='new comment',
-            submit_date=now,
-            object_pk=self.project.id,
-            content_type_id=self.project.id,
-            site_id=1,
-        )
-        comment.save()
+        resp.form['comment'] = 'new comment'
+        resp = resp.form.submit()
+
         resp = self.app.get('/projects/test-project/', user=self.user)
         self.assertEqual(resp.status_int, 200)
 
