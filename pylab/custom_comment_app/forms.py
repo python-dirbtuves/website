20f619dad93c3783a43b88ee3b7715288dec0704
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-27 07:36:08 +0300

    Remove custom_comment_app
    It was removed, because there is no need to have custom parent field. Parent
    can be stored in default django_comment.Comment.content_object field.

--- pylab/custom_comment_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django_comments.forms import CommentForm
-from .models import ParentComment
-
-
-class ParentCommentForm(CommentForm):
-
-    def get_comment_model(self):
-        return ParentComment
