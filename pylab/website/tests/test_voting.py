8eb7d5e69cb9c05c9ba21c64b7b88481de54c9a0
vomax <spurguoliukas@gmail.com>
2015-08-29 09:07:48 -0700

    Expand voting poll test by testing voting poll details page

--- pylab/website/tests/test_voting.py
+++ pylab/website/tests/test_voting.py
@@ -9,8 +9,9 @@ from pylab.core.models import Project, VotingPoll, Vote
 
 class VotingTests(WebTest):
 
-    def test_voting_page(self):
+    def test_voting_and_voting_poll_details_page(self):
         u1 = User.objects.create(username='u1')
+        u2 = User.objects.create(username='u2')
 
         p1 = Project.objects.create(author=u1, title='Test title 1', description='Test description')
         p2 = Project.objects.create(author=u1, title='Test title 2', description='Test description')
@@ -19,6 +20,8 @@ class VotingTests(WebTest):
 
         Vote.objects.create(voter=u1, voting_poll=vp, project=p1)
         Vote.objects.create(voter=u1, voting_poll=vp, project=p2)
+        Vote.objects.create(voter=u2, voting_poll=vp, project=p1)
+        Vote.objects.create(voter=u2, voting_poll=vp, project=p2)
 
         resp = self.app.get('/vote/test-voting-poll/', user='u1')
         self.assertEqual(resp.status_int, 200)
@@ -32,18 +35,32 @@ class VotingTests(WebTest):
 
         time_after_vote = datetime.datetime.now()
 
-        self.assertEqual(list(Vote.objects.values_list('points', flat=True)), [3, 2])
+        self.assertEqual(list(Vote.objects.filter(voter=u1).values_list('points', flat=True)), [3, 2])
 
-        for v in Vote.objects.all():
+        for v in Vote.objects.filter(voter=u1):
             self.assertLess(time_before_vote, v.voted)
             self.assertGreater(time_after_vote, v.voted)
 
-        resp = self.app.get('/vote/test-voting-poll/', user='u1')
+        resp = self.app.get('/vote/test-voting-poll/', user='u2')
         self.assertEqual(resp.status_int, 200)
 
         time_before_vote = datetime.datetime.now()
 
-        resp.form['form-0-points'].value = 30  # Vote points sum should be less or equal to 15
-        resp.form['form-1-points'].value = 20
+        resp.form['form-0-points'].value = 6  # Vote points sum should be less or equal to 15
+        resp.form['form-1-points'].value = 5
         resp = resp.form.submit()
+        self.assertEqual(resp.status_int, 302)
+
+        time_after_vote = datetime.datetime.now()
+
+        self.assertEqual(list(Vote.objects.filter(voter=u2).values_list('points', flat=True)), [6, 5])
+
+        for v in Vote.objects.filter(voter=u2):
+            self.assertLess(time_before_vote, v.voted)
+            self.assertGreater(time_after_vote, v.voted)
+
+        resp = self.app.get('/voting-poll/test-voting-poll/', user='u1')
         self.assertEqual(resp.status_int, 200)
+
+        self.assertTrue(b'9' in resp.content)
+        self.assertTrue(b'7' in resp.content)
